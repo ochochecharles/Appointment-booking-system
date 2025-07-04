54d68f138f6a78613b3d47695606574c2b1e5047
ochochecharles <ochochecharles@gmail.com>
2025-07-04 01:13:33 +0100

    Fixed environmental variables

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.VITE_AUTH_DOMAIN,
-  projectId: import.meta.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.VITE_MEASUREMENT_ID
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
